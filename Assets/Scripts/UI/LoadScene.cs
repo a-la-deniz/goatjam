d06563a88c54b23260b7bf5972ce581bf3643bbb
Lorena Weder <latamakuchi@gmail.com>
2021-01-30 13:56:42 +0100

    Project Settings, instructions scene

---
+++ Assets/Scripts/UI/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+	[SerializeField] private int _scene;
+
+	public void Load()
+	{
+		SceneManager.LoadScene(_scene);
+	}
+}

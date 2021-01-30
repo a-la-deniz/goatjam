7fa5e3877e5a42c68956bbfd6224c05ec83fd454
Deniz Aladagli <alidenizaladagli@hotmail.com>
2021-01-30 01:24:01 +0100

    Added adjustable cone

--- Assets/Scripts/GoatController.cs
+++ Assets/Scripts/GoatController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class GoatController : MonoBehaviour
 {
-	[SerializeField] private float _speed = 1f;
+	[SerializeField] private float      _speed = 1f;
 	[SerializeField] private GameObject _cone;
 
 	private Rigidbody2D _rigidbody2D;
+
+	public GameObject Cone => _cone;
+
 	private void Awake()
 	{
 		_rigidbody2D = GetComponent<Rigidbody2D>();
@@ -24,7 +27,5 @@ public class GoatController : MonoBehaviour
 
 		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
 		_cone.transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
-
-
 	}
 }
